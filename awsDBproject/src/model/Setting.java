90b97794ac008f2847169b9913905c73ce1c0c71
hamcheeseburger <yoo77hyeon@gmail.com>
2021-01-26 00:47:43 +0900

    Setting DTO에 id 변수 추가

--- awsDBproject/src/model/Setting.java
+++ awsDBproject/src/model/Setting.java
@@ -1,16 +1,22 @@
 package model;
 
 public class Setting {
-	private int DETECT_SEC;
+	private int STG_ID;
+	private int DETEC_SEC;
 	private int NOD_SEC;
-	private int RECOV_LV;
+	private int RECOG_LV;
 	
-	
-	public int getDETECT_SEC() {
-		return DETECT_SEC;
+	public int getSTG_ID() {
+		return STG_ID;
 	}
-	public void setDETECT_SEC(int dETECT_SEC) {
-		DETECT_SEC = dETECT_SEC;
+	public void setSTG_ID(int sTG_ID) {
+		STG_ID = sTG_ID;
+	}
+	public int getDETEC_SEC() {
+		return DETEC_SEC;
+	}
+	public void setDETEC_SEC(int dETEC_SEC) {
+		DETEC_SEC = dETEC_SEC;
 	}
 	public int getNOD_SEC() {
 		return NOD_SEC;
@@ -18,11 +24,11 @@ public class Setting {
 	public void setNOD_SEC(int nOD_SEC) {
 		NOD_SEC = nOD_SEC;
 	}
-	public int getRECOV_LV() {
-		return RECOV_LV;
+	public int getRECOG_LV() {
+		return RECOG_LV;
 	}
-	public void setRECOV_LV(int rECOV_LV) {
-		RECOV_LV = rECOV_LV;
+	public void setRECOV_LV(int rECOG_LV) {
+		RECOG_LV = rECOG_LV;
 	}
 	
 }
