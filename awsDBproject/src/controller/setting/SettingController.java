22698d8b8af648f65beec6589635c38f3286f214
hamcheeseburger <yoo77hyeon@gmail.com>
2021-01-27 18:41:56 +0900

    수정

--- awsDBproject/src/controller/setting/SettingController.java
+++ awsDBproject/src/controller/setting/SettingController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
 import model.Member;
+import model.Setting;
 import model.service.Manager;
 
 public class SettingController implements Controller{
@@ -12,7 +13,14 @@ public class SettingController implements Controller{
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
 		
+		Manager manager = Manager.getInstance();
+		Setting setting = manager.getSetting();
 		
+		if(setting == null) {
+			return "setting/sendFail.jsp";
+		}
+		
+		request.setAttribute("setting", setting);
 		return "/setting/sendSetting.jsp";
 	}
 
